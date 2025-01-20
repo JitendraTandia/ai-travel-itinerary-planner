36a0f2c99ba249f04615611fc24448dc4fee594b
TheNamXe0n <thenameet0@gmail.com>
2025-01-20 11:28:37 +0530

    people and chat page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import PeoplePage from "./components/people/PeoplePage";
 import RecentTrips from "./components/RecentTrips";
 import TripItinerary from "./components/TripItinerary";
 import Layout from "./Layout/Layout";
@@ -7,11 +8,12 @@ import Layout from "./Layout/Layout";
 function App() {
   return (
       <Layout>
-        <div className="bg-bg pb-7 h-screen flex flex-col justify-end items-center  bg-cover bg-no-repeat backdrop-contrast-75">
+        {/* <div className="bg-bg pb-7 h-screen flex flex-col justify-end items-center  bg-cover bg-no-repeat backdrop-contrast-75">
           <Hero />
           <RecentTrips />
-        </div>
+        </div> */}
           {/* <TripItinerary/> */}
+          <PeoplePage/>
       </Layout>
     // </div>
   );
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import TabNavBar from '../components/TabNavBar'
 
 function Layout( {children} ) {
+  const pathName = window.location.pathname
+
   return (
     <div>
         <Navbar/>
-        <main className='min-h-[70vh]'> {children} </main>
+        <main className='min-h-[70vh] pt-20'> {children} 
+        {pathName!=="/" && <TabNavBar/>}
+        </main>
+
         
     </div>
   )
